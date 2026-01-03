92beff9eec19fbd5c8600c1c64ce3e941cc7d82c
Felbrou <felbrou@gmail.com>
2026-01-03 12:31:40 -0300

    final-project: added bennefits section; cta section and footer

---
+++ src/app/components/benefits-section/benefits-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'benefits-section',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './benefits-section.component.html',
+  styleUrl: './benefits-section.component.css'
+})
+export class BenefitsSectionComponent {
+
+}
